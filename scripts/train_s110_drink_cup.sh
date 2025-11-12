43eb7c8ad3ef0ca58dc8c64d71dabd1c1a8c357c
youngwoon <duddns2048@korea.ac.kr>
2025-11-13 02:33:09 +0900

    fig files

---
+++ scripts/train_s110_drink_cup.sh
@@ -0,0 +1,31 @@
+CUDA_VISIBLE_DEVICES=6 python skillmimic/run.py \
+  --experiment Drink_from_cup\
+  --task  SkillMimicParahomeLocalHistRISBuffernode\
+  --episode_length 60 \
+  --cfg_env skillmimic/data/cfg/parahome_sm/parahome_s110_drink_dup_hist60_noisyinit_simpara.yaml \
+  --cfg_train skillmimic/data/cfg/train/rlg/parahome.yaml \
+  --motion_file skillmimic/data/motions/ParaHome/Drink_from_cup/ \
+  --reweight --reweight_alpha 1.0 \
+  --state_init_random_prob 0.1 \
+  --state_switch_prob 0.0 \
+  --hist_length 60 \
+  --history_embedding_size 3 \
+  --hist_ckpt hist_encoder/ParaHome/hist_model.ckpt \
+  --obj_trans /home/youngwoon/github/v2_SkillMimic/seq/s110/object_transformations.pkl \
+  --headless \
+  --resume_from output/Drink_from_cup_12-08-44-17/nn/Drink_from_cup_e1825_r19.5359.pth
+
+    # parahome_place_kettle_hist60_noisyinit_simpara.yaml 
+    #   in_scene_obj_static
+    #   in_scene_obj_dynamic
+    # --experiment
+    # --obj_trans
+    # --resume_from
+    # CUDA_DEVICE
+    # --motion_file skillmimic/data/motions/Drink_from_cup/ \
+    # --motion_file skillmimic/data/motions/Move_book_from_desk_to_bookshelf/ \
+    # --motion_file skillmimic/data/motions/Move_chair_from_desk_to_table/ \
+    # --motion_file skillmimic/data/motions/Move_cup_from_desk_to_table/ \
+    # --motion_file skillmimic/data/motions/Move_cup_from_table_to_desk/ \
+    # --motion_file skillmimic/data/motions/Move_kettle_from_table_to_desk/ \
+    # --motion_file skillmimic/data/motions/Move_pan_from_gas_stove_to_table/ \
---
+++ scripts/train_s117_pan_diningtable2gasstove copy.sh
@@ -0,0 +1,30 @@
+CUDA_VISIBLE_DEVICES=7 python skillmimic/run.py \
+  --experiment Move_pan_from_gas_stove_to_table\
+  --task  SkillMimicParahomeLocalHistRISBuffernode\
+  --episode_length 60 \
+  --cfg_env skillmimic/data/cfg/parahome_sm/parahome_s117_pan_diningtable2gasstove_hist60_noisyinit_simpara.yaml \
+  --cfg_train skillmimic/data/cfg/train/rlg/parahome.yaml \
+  --motion_file skillmimic/data/motions/ParaHome/Move_pan_from_gas_stove_to_table/ \
+  --reweight --reweight_alpha 1.0 \
+  --state_init_random_prob 0.1 \
+  --state_switch_prob 0.0 \
+  --hist_length 60 \
+  --history_embedding_size 3 \
+  --hist_ckpt hist_encoder/ParaHome/hist_model.ckpt \
+  --obj_trans /home/youngwoon/github/v2_SkillMimic/seq/s117/object_transformations.pkl \
+  --headless \
+  --resume_from output/Move_pan_from_gas_stove_to_table_12-08-47-19/nn/Move_pan_from_gas_stove_to_table_e332_r11.1825.pth
+
+    # parahome_place_kettle_hist60_noisyinit_simpara.yaml 
+    #   in_scene_obj_static
+    #   in_scene_obj_dynamic
+    # --experiment
+    # --obj_trans
+    # --resume_from
+    # --motion_file skillmimic/data/motions/Drink_from_cup/ \
+    # --motion_file skillmimic/data/motions/Move_book_from_desk_to_bookshelf/ \
+    # --motion_file skillmimic/data/motions/Move_chair_from_desk_to_table/ \
+    # --motion_file skillmimic/data/motions/Move_cup_from_desk_to_table/ \
+    # --motion_file skillmimic/data/motions/Move_cup_from_table_to_desk/ \
+    # --motion_file skillmimic/data/motions/Move_kettle_from_table_to_desk/ \
+    # --motion_file skillmimic/data/motions/Move_pan_from_gas_stove_to_table/ \
